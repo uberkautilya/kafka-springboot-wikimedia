73544d4cb1116b5111bf963f4c509e51daf2f8e8
Mithun <mithuncm@gmail.com>
2023-01-13 17:07:00 +0530

    Created WikimediaConsumerApplication module and added its properties

---
+++ kafka-consumer-wikimedia/src/main/java/uber/kautilya/wikimediaconsumer/WikimediaConsumerApplication.java
@@ -0,0 +1,11 @@
+package uber.kautilya.wikimediaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WikimediaConsumerApplication.class);
+    }
+}
