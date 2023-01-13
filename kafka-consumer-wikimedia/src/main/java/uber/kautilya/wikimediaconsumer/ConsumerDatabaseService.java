f25f441986e8fb2bdd5947837c568406fac42948
Mithun <mithuncm@gmail.com>
2023-01-13 17:21:46 +0530

    Added a Kafka Listener service to kafka-consumer-wikimedia module

---
+++ kafka-consumer-wikimedia/src/main/java/uber/kautilya/wikimediaconsumer/ConsumerDatabaseService.java
@@ -0,0 +1,16 @@
+package uber.kautilya.wikimediaconsumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerDatabaseService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerDatabaseService.class);
+
+    @KafkaListener(topics = "wikimedia_events", groupId = "myConsumerGroup")
+    public void consume(String eventMessage) {
+        LOGGER.info("Consumer eventMessage: " + eventMessage);
+    }
+}
